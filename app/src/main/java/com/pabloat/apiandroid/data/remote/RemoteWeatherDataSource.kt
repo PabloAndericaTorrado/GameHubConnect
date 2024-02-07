17f549deeef9d732420071fc179ccba518b05ab7
Pablo <pabloat@iescastelar.com>
2024-02-07 09:49:41 +0100

    Empezando de 0

--- app/src/main/java/com/pabloat/apiandroid/data/remote/RemoteWeatherDataSource.kt
+++
@@ -1,9 +0,0 @@
-package com.pabloat.apiandroid.data.remote
-
-import com.pabloat.apiandroid.data.local.Producto
-import com.pabloat.apiandroid.data.remote.ApiService
-
-class RemoteWeatherDataSource(private val apiService: ApiService) {
-    suspend fun getCategoria(id: Int, name: String, products: Producto) =
-        apiService.getCategoria(id, name, products)
-}
--- app/src/main/java/com/pabloat/apiandroid/navigation/Destinations.kt
+++
@@ -1,7 +0,0 @@
-package com.pabloat.apiandroid.navigation
-
-sealed class Destinations(val route: String) {
-    object MainScreen : Destinations(route = "main_screen")
-    object ListScreen : Destinations(route = "list_screen")
-}
-
