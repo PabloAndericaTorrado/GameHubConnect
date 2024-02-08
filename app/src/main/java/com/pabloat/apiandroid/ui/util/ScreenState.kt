6c59b540d884fe64d73971cfc0d1418c8e8611e7
Pablo <pabloat@iescastelar.com>
2024-02-08 10:28:44 +0100

    ScreenState

--- app/src/main/java/com/pabloat/apiandroid/ui/util/ScreenState.kt
+++ app/src/main/java/com/pabloat/apiandroid/ui/util/ScreenState.kt
@@ -1,10 +1,10 @@
 package com.pabloat.apiandroid.ui.util
 
-import com.pabloat.apiandroid.data.local.Categoria
+import com.pabloat.apiandroid.data.local.Videojuego
 import kotlinx.coroutines.flow.Flow
 
 sealed class ScreenState {
     object Loading : ScreenState()
     data class Error(val message: String) : ScreenState()
-    data class Success(val weathers: Flow<List<Categoria>>) : ScreenState()
+    data class Success(val weathers: Flow<List<Videojuego>>) : ScreenState()
 }
