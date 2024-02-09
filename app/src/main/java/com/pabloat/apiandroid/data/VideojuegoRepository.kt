e55a6c09e0afbf4a73c9d8d733970518b67e6f5a
FernandoBaqueroo <fernandobz@iescastelar.com>
2024-02-09 02:53:36 +0100

    Refactorización de los datos recibidos de la API, devuelve una lista y no recibe ningún dato por parámetro.
    El tratamiento de los datos en el VideojuegoRepository se hace directamente en la función getRemoteVideojuego para insertarlo
    a la BBDD con un insert por lotes(varios registros en una sentencia sql / una lista)

--- app/src/main/java/com/pabloat/apiandroid/data/VideojuegoRepository.kt
+++ app/src/main/java/com/pabloat/apiandroid/data/VideojuegoRepository.kt
@@ -3,7 +3,6 @@ package com.pabloat.apiandroid.data
 import com.pabloat.apiandroid.data.local.VideojuegoDatasource
 import com.pabloat.apiandroid.data.local.Videojuego
 import com.pabloat.apiandroid.data.remote.RemoteVideojuegoDataSource
-import com.pabloat.apiandroid.data.remote.VideoJuegosDTO
 import com.pabloat.apiandroid.data.remote.toLocalList
 import kotlinx.coroutines.flow.Flow
 
@@ -12,11 +11,18 @@ class VideojuegoRepository(
     private val remoteds: RemoteVideojuegoDataSource
 ) {
     suspend fun getRemoteVideojuego(
-        id: Int, title: String, developer: String, shortDescription: String, genre: String, platform: String, date: String
-    ): List<Videojuego> {
-        val videojuego: Videojuego = Videojuego(0,"","","","","","")
 
-        return remoteds.getVideojuegos(id, title, developer, shortDescription, genre, platform, date).toLocalList()
+    ): List<Videojuego> {
+        val videojuegos = remoteds.getVideojuegos()
+        val videojuegosLocales = ArrayList<Videojuego>()
+
+        for (videojuego in videojuegos) {
+            videojuegosLocales.add(videojuego.toVideojuego())
+        }
+        localds.insertVideojuego(videojuegosLocales)
+        return videojuegosLocales
+        //val listaVideojuegos = remoteds.getVideojuegos().toLocalList()
+        //return listaVideojuegos
     }
 
     suspend fun getLocalVideojuego(): Flow<List<Videojuego>>{
