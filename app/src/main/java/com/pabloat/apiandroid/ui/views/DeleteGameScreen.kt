4ffce4ee2f206ca60b1d0d7fbf211eea897681e6
FernandoBaqueroo <fernandobz@iescastelar.com>
2024-02-14 19:02:46 +0100

    Asíncronia + Limpieza de código. La funcionalidad de borrado funciona correctamente de forma asíncrona y se ha refactorizado el código de manera que queda más limpio y legible.

--- app/src/main/java/com/pabloat/apiandroid/ui/views/DeleteGameScreen.kt
+++ app/src/main/java/com/pabloat/apiandroid/ui/views/DeleteGameScreen.kt
@@ -7,7 +7,9 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.Button
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Snackbar
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -16,8 +18,8 @@ import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
 import com.pabloat.apiandroid.data.local.Videojuego
 import com.pabloat.apiandroid.ui.util.VideojuegosDeleteItem
-import com.pabloat.apiandroid.ui.util.VideojuegosItem
 import com.pabloat.apiandroid.viewmodel.MainViewModel
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
 
 @Composable
@@ -25,34 +27,78 @@ fun DeleteGameScreen(onNavController: NavHostController, mainViewModel: MainView
     val texto = remember { mutableStateOf("") }
     val videojuegoEncontrado = remember { mutableStateOf<Videojuego?>(null) }
     val coroutineScope = rememberCoroutineScope()
+    val showSnackbar = remember { mutableStateOf(false) }
 
     Column (
         modifier = Modifier.fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
-        OutlinedTextField(
-            value = texto.value,
-            onValueChange = { texto.value = it },
-            label = { Text("Título") },
-            modifier = Modifier.fillMaxWidth()
-        )
+        BuscarJuegoTextField(texto, mainViewModel, coroutineScope, videojuegoEncontrado)
+        MostrarBotonEliminar(videojuegoEncontrado, mainViewModel, coroutineScope, showSnackbar)
+        MostrarSnackbar(showSnackbar)
+    }
+}
 
-        Button(onClick = {
-            coroutineScope.launch {
-                val videojuego = mainViewModel.searchGame(texto.value)
-                videojuegoEncontrado.value = videojuego
-            }
-        }) {
-            Text("Buscar juego")
+
+/**
+ * MÉTODOS PARA LA PANTALLA DEL BORRADO
+ */
+@Composable
+private fun BuscarJuegoTextField(
+    texto: MutableState<String>,
+    mainViewModel: MainViewModel,
+    coroutineScope: CoroutineScope,
+    videojuegoEncontrado: MutableState<Videojuego?>
+) {
+    OutlinedTextField(
+        value = texto.value,
+        onValueChange = { texto.value = it },
+        label = { Text("Título") },
+        modifier = Modifier.fillMaxWidth()
+    )
+
+    Button(onClick = {
+        coroutineScope.launch {
+            val videojuego = mainViewModel.searchGame(texto.value)
+            videojuegoEncontrado.value = videojuego
         }
+    }) {
+        Text("Buscar juego")
+    }
+}
 
-        videojuegoEncontrado.value?.let { videojuego ->
-            VideojuegosDeleteItem(videojuego = videojuego, onDeleteClick = {
-                coroutineScope.launch {
-                    videojuego.id?.let { mainViewModel.deleteGame(it) }
+@Composable
+private fun MostrarBotonEliminar(
+    videojuegoEncontrado: MutableState<Videojuego?>,
+    mainViewModel: MainViewModel,
+    coroutineScope: CoroutineScope,
+    showSnackbar: MutableState<Boolean>
+) {
+    videojuegoEncontrado.value?.let { videojuego ->
+        VideojuegosDeleteItem(videojuego = videojuego, onDeleteClick = {
+            coroutineScope.launch {
+                videojuego.id?.let {
+                    mainViewModel.deleteGame(it)
+                    videojuegoEncontrado.value = null
+                    showSnackbar.value = true
                 }
-            })
+            }
+        })
+    }
+}
+
+@Composable
+private fun MostrarSnackbar(showSnackbar: MutableState<Boolean>) {
+    if (showSnackbar.value) {
+        Snackbar(
+            action = {
+                TextButton(onClick = { showSnackbar.value = false }) {
+                    Text("Cerrar")
+                }
+            }
+        ) {
+            Text("El videojuego ha sido borrado")
         }
     }
 }
